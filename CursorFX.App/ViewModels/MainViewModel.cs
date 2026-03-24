9816c045f1f8c19a057ac903cf0d146012e24625
LiveriaDev <liveriadev@gmail.com>
2026-03-24 16:02:32 +0200

    Bump app and installer version to 0.0.2

--- CursorFX.App/ViewModels/MainViewModel.cs
+++ CursorFX.App/ViewModels/MainViewModel.cs
@@ -204,7 +204,7 @@ public sealed class MainViewModel : INotifyPropertyChanged, IDisposable
 
     public string PluginAuthoringGuidePath => Path.Combine(AppContext.BaseDirectory, "Templates", "plugin-authoring-guide.txt");
 
-    public string ApplicationVersion => "v0.0.1";
+    public string ApplicationVersion => "v0.0.2";
 
     public string ApplicationAuthor => "Dokzya_dev";
 
